621700e57a91b7a024753dbf69d52312091bc380
mziyadam <mziyadam@users.noreply.github.com>
2023-08-24 12:02:01 +0700

    ubah docs

--- src/pegawai/pegawai.service.ts
+++ src/pegawai/pegawai.service.ts
@@ -15,43 +15,16 @@ export class PegawaiService {
   create(createPegawaiDto: CreatePegawaiDto) {
     return 'This action adds a new pegawai';
   }
-
-  findAll(query, kode_satker_awal): Promise<Pegawai[]> {
+  //TODO kasi total
+  // findAll(query, kode_satker_awal): Promise<Pegawai[]> {
+  async findAll(query, kode_satker_awal): Promise<any> {
     const satker = query.satker || null;
     const take = query.limit || 10;
     const page = query.page || 1;
     const skip = (page - 1) * take;
-    if(kode_satker_awal=='0000'){
+    if (kode_satker_awal == '0000') {
       if (satker != null) {
-          return this.pegawaiRepository.find({
-            order: {
-              NIP: "ASC",
-            },
-            where: [
-              {
-                KODE_SATUAN_KERJA: satker
-              }
-              // ,{
-              //   KODE_SATUAN_KERJA:Like(kode_satker_awal+'%')
-              // }
-            ],
-            skip: skip,
-            take: take
-          });
-      }
-      else
-        return this.pegawaiRepository.find({
-          order: {
-            NIP: "ASC",
-          },
-          skip: skip,
-          take: take
-        });
-    }else{
-      
-    if (satker != null) {
-      if (satker.substring(0, 4) == kode_satker_awal)
-        return this.pegawaiRepository.find({
+        const data = await this.pegawaiRepository.find({
           order: {
             NIP: "ASC",
           },
@@ -66,29 +39,133 @@ export class PegawaiService {
           skip: skip,
           take: take
         });
-      else
-        return null
-    }
-    else
-      return this.pegawaiRepository.find({
-        order: {
-          NIP: "ASC",
-        },
-        skip: skip,
-        take: take,
-        where: {
-          KODE_SATUAN_KERJA: Like(kode_satker_awal + '%')
+
+        const dataLength = await this.pegawaiRepository.query("select count(*) as total from temp_pegawai where kode_satuan_kerja=@0", [satker]);
+        // const rawData= await this.pegawaiRepository.find({
+        //   order: {
+        //     NIP: "ASC",
+        //   },
+        //   where: [
+        //     {
+        //       KODE_SATUAN_KERJA: satker
+        //     }
+        //     // ,{
+        //     //   KODE_SATUAN_KERJA:Like(kode_satker_awal+'%')
+        //     // }
+        //   ],
+        // });
+        return {
+          total: dataLength[0].total,
+          total_page: Math.ceil(dataLength[0].total / take), current_page: page,
+          limit: take,
+          data: data
         }
-      });
+      }
+      else {
+        const data = await this.pegawaiRepository.find({
+          order: {
+            NIP: "ASC",
+          },
+          skip: skip,
+          take: take
+        });
+
+        const dataLength = await this.pegawaiRepository.query("select count(*) as total from temp_pegawai");
+        // const rawData = await this.pegawaiRepository.find({
+        //   order: {
+        //     NIP: "ASC",
+        //   },
+        // });
+        return {
+          total: dataLength[0].total,
+          total_page: Math.ceil(dataLength[0].total / take), current_page: page,
+          limit: take,
+          data: data
+        }
+      }
+    } else {
+      if (satker != null) {
+        if (satker.substring(0, 4) == kode_satker_awal) {
+          const data = await this.pegawaiRepository.find({
+            order: {
+              NIP: "ASC",
+            },
+            where: [
+              {
+                KODE_SATUAN_KERJA: satker
+              }
+              // ,{
+              //   KODE_SATUAN_KERJA:Like(kode_satker_awal+'%')
+              // }
+            ],
+            skip: skip,
+            take: take
+          });
+
+          const dataLength = await this.pegawaiRepository.query("select count(*) as total from temp_pegawai where kode_satuan_kerja=@0", [satker]);
+          // const rawData = await this.pegawaiRepository.find({
+          //   order: {
+          //     NIP: "ASC",
+          //   },
+          //   where: [
+          //     {
+          //       KODE_SATUAN_KERJA: satker
+          //     }
+          //     // ,{
+          //     //   KODE_SATUAN_KERJA:Like(kode_satker_awal+'%')
+          //     // }
+          //   ],
+          // });
+          return {
+            total: dataLength[0].total,
+            total_page: Math.ceil(dataLength[0].total / take), current_page: page,
+            limit: take,
+            data: data
+          }
+        }
+        else {
+          return []
+        }
+      }
+      else {
+        const data = await this.pegawaiRepository.find({
+          order: {
+            NIP: "ASC",
+          },
+          skip: skip,
+          take: take,
+          where: {
+            KODE_SATUAN_KERJA: Like(kode_satker_awal + '%')
+          }
+        });
+
+        const dataLength = await this.pegawaiRepository.query("select count(*) as total from temp_pegawai where kode_satuan_kerja like @0+'%'", [satker]);
+        // const rawData = await this.pegawaiRepository.find({
+        //   order: {
+        //     NIP: "ASC",
+        //   },
+        //   skip: skip,
+        //   take: take,
+        //   where: {
+        //     KODE_SATUAN_KERJA: Like(kode_satker_awal + '%')
+        //   }
+        // });
+        return {
+          total: dataLength[0].total,
+          total_page: Math.ceil(dataLength[0].total / take), current_page: page,
+          limit: take,
+          data: data
+        }
+      }
     }
   }
 
   findOne(nip: string, kode_satker_awal: string): Promise<Pegawai> {
-    if(kode_satker_awal=='0000'){
+    if (kode_satker_awal == '0000') {
       return this.pegawaiRepository.findOne({
         where: { NIP_BARU: Equal(nip) }
       });
-    }else{
+    } else {
       return this.pegawaiRepository.findOne({
         where: { NIP_BARU: Equal(nip), KODE_SATUAN_KERJA: Like(kode_satker_awal + '%') }
       });
@@ -100,7 +177,7 @@ export class PegawaiService {
     // return [kode_satker_awal];
     return await this.pegawaiRepository.query("exec get_pendidikan_by_nip_baru @0,@1", [nip, kode_satker_awal]);
   }
-//
+  //
   async findOnePekerjaan(nip: string, kode_satker_awal: string): Promise<any> {
     const pangkat = await this.pegawaiRepository.query("exec get_pangkat_by_nip_baru @0,@1", [nip, kode_satker_awal]);
     const jabatan = await this.pegawaiRepository.query("exec get_jabatan_by_nip_baru @0,@1", [nip, kode_satker_awal]);
